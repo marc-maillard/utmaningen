62586105faca0bf35d32a154281a6271efcce99e
Marc Maillard <marc.maillard@edpconsult.se>
2020-10-16 15:52:44 +0200

    having a hard time with css on the logEntries

--- src/components/challenges/SkeletonCard.js
+++ src/components/challenges/SkeletonCard.js
@@ -1,22 +1,36 @@
-import React from 'react';
+import React from "react";
 
 import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
 import { Grid } from "@material-ui/core";
-import Skeleton from "@material-ui/lab/Skeleton"
+import Skeleton from "@material-ui/lab/Skeleton";
 
 function SkeletonCard(props) {
-    return (
-        <Grid item>
+  return (
+    <Grid item>
       <Card>
-          <CardHeader
-            avatar={<Skeleton animation="wave" variant="circle" width={40} height={40} />}
-            title={<Skeleton animation="wave" height={10} width="80%" style={{ marginBottom: 6 }} />}
-            subheader={<Skeleton animation="wave" height={10} width="40%" />}
-          />
+        <CardHeader
+          avatar={
+            <Skeleton
+              animation="wave"
+              variant="circle"
+              width={40}
+              height={40}
+            />
+          }
+          title={
+            <Skeleton
+              animation="wave"
+              height={10}
+              width="80%"
+              style={{ marginBottom: 6 }}
+            />
+          }
+          subheader={<Skeleton animation="wave" height={10} width="40%" />}
+        />
       </Card>
     </Grid>
-    );
+  );
 }
 
 export default SkeletonCard;
--- src/components/logEntries/LogEntryButtons.js
+++ src/components/logEntries/LogEntryButtons.js
@@ -1,10 +1,25 @@
 import React, { useEffect, useState } from "react";
 
+import { makeStyles } from "@material-ui/core/styles";
 import ToggleButton from "@material-ui/lab/ToggleButton";
 import ToggleButtonGroup from "@material-ui/lab/ToggleButtonGroup";
-import { Chip, Typography } from "@material-ui/core";
+import { Box, Chip, Grid, Typography } from "@material-ui/core";
+import DirectionsWalkIcon from "@material-ui/icons/DirectionsWalk";
+import HotelIcon from "@material-ui/icons/Hotel";
+import BeachAccessIcon from "@material-ui/icons/BeachAccess";
+import SentimentVerySatisfiedIcon from "@material-ui/icons/SentimentVerySatisfied";
+
+const useStyles = makeStyles((theme) => ({
+  ToggleButton: {
+    height: 80,
+  },
+  ToggleButtonIcon: {
+    marginRight: 5,
+  },
+}));
 
 function LogEntryButtons(props) {
+  const classes = useStyles();
   const [entries, setEntries] = useState(() => []);
   const [superPowerDisabled, setSuperPowerAsDisabled] = useState(true);
   const [resting, setResting] = useState("");
@@ -48,26 +63,52 @@ function LogEntryButtons(props) {
 
   return (
     <React.Fragment>
-      <ToggleButtonGroup value={entries} onChange={handleEntries}>
-        <ToggleButton value="main">
-          <Typography>30'</Typography>
-        </ToggleButton>
-        <ToggleButton value="xtra">
-          <Typography>Xtra</Typography>
-        </ToggleButton>
-        <ToggleButton value="superPower" disabled={superPowerDisabled}>
-          <Typography>60'</Typography>
-        </ToggleButton>
-      </ToggleButtonGroup>
-      <ToggleButtonGroup exclusive value={resting} onChange={handleRestingDay}>
-        <ToggleButton value="sickDay">
-          <Typography>Sick day</Typography>
-        </ToggleButton>
-        <ToggleButton value="dayOff">
-          <Typography>Day off</Typography>
-        </ToggleButton>
-      </ToggleButtonGroup>
-      <Chip label={score} />
+      <Box mx={3} mt={3}>
+        <Grid container alignItems="center" direction={"column"} spacing={2}>
+          <Grid item>
+            <ToggleButtonGroup
+              className={classes.ToggleButton}
+              value={entries}
+              onChange={handleEntries}
+            >
+              <ToggleButton value="main">
+                <DirectionsWalkIcon className={classes.ToggleButtonIcon} />
+                <Typography>30'</Typography>
+              </ToggleButton>
+              <ToggleButton value="xtra">
+                <SentimentVerySatisfiedIcon
+                  className={classes.ToggleButtonIcon}
+                />
+                <Typography>New healthy habit</Typography>
+              </ToggleButton>
+              <ToggleButton value="superPower" disabled={superPowerDisabled}>
+                <DirectionsWalkIcon className={classes.ToggleButtonIcon} />
+                <Typography>60'</Typography>
+              </ToggleButton>
+            </ToggleButtonGroup>
+          </Grid>
+          <Grid item>
+            <ToggleButtonGroup
+              className={classes.ToggleButton}
+              exclusive
+              value={resting}
+              onChange={handleRestingDay}
+            >
+              <ToggleButton value="sickDay">
+                <HotelIcon className={classes.ToggleButtonIcon} />
+                <Typography>Sick day</Typography>
+              </ToggleButton>
+              <ToggleButton value="dayOff">
+                <BeachAccessIcon className={classes.ToggleButtonIcon} />
+                <Typography>Day off</Typography>
+              </ToggleButton>
+            </ToggleButtonGroup>
+          </Grid>
+          <Grid item>
+            <Chip label={score} />
+          </Grid>
+        </Grid>
+      </Box>
     </React.Fragment>
   );
 }
